e518d8ab3fa52f024ee445c75114753dd6e3fb46
Worldfrom6feet <dhruvilmaniar@gmail.com>
2020-04-16 02:23:32 +0530

    Updated Shell script

--- run.sh
+++ run.sh
@@ -1,46 +1,33 @@
 export MODE=$1
 
-HELP()
-{
-    echo "Run Python Module to fetch Notifications from University website."
-    echo
-    echo "Syntax : run.sh [1|2|3]"
-    echo "Options:"
-    echo "1    Print data to console only."
-    echo "2    Write data to a text file."
-    echo "3    Download all notifications pdf files."
-    echo
-    echo "Text files and PDFs will be saved in ./Data Folder."
-    echo
-}
+if [ $# -eq 0 ]
+then
+    echo "########################################################################"
+    echo "##    No Arguments were passed.                                         "
+    echo "##"
+    echo "##    Run Python Module to fetch Notifications from University website."
+    echo "##"
+    echo "##    Syntax : run.sh 1|2|3"
+    echo "##    Options:"
+    echo "##    1    Print data to console only."
+    echo "##    2    Write data to a text file."
+    echo "##    3    Download all notifications pdf files."
+    echo "##"
+    echo "##    Text files and PDFs will be saved in ./Data Folder."
+    echo "##"
+    echo "########################################################################"
 
-while getopts ":h" option
-do
-    case $option in
-        h)
-            HELP
-            exit;;
-        a)
-            echo "ONE"
-            exit;;
-        -2)
-            echo "TWO"
-            exit;;
-        # \?)
-        #     echo "NONE"
-        #     exit;;
-    esac
-done
-
-# if (($MODE == "1"))
-# then
-#     python -m src.UniversityFetch 1
-# elif (($MODE == "2"))
-# then
-#     python -m src.UniversityFetch 2
-# elif (($MODE == "3"))
-# then
-#     python -m src.UniversityFetch 3
-# else
-#     echo "Argument not supported yet.."
-# fi
+else
+    if (($MODE == "1"))
+    then
+        python -m src.UniversityFetch 1
+    elif (($MODE == "2"))
+    then
+        python -m src.UniversityFetch 2
+    elif (($MODE == "3"))
+    then
+        python -m src.UniversityFetch 3
+    else
+        echo "Wrong option!"
+    fi
+fi
